897dda10908820864e1e6e71fe59b966da6da2eb
alwaysstudying <86540250+alwaysstudying@users.noreply.github.com>
2021-08-11 13:59:10 +0900

    2021.08.11 병합작업

---
+++ src/main/java/com/team04/restaurant/RestaurantService.java
@@ -0,0 +1,23 @@
+package com.team04.restaurant;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class RestaurantService implements RestaurantDao {
+
+	@Override
+	public List<RestaurantDto> selectrestaurantList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RestaurantDto selectRestaurant(String shopNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
