951e73d998bd7faffb2f32b55eba3787ba8dffc2
alwaysstudying <86540250+alwaysstudying@users.noreply.github.com>
2021-08-13 10:04:55 +0900

    변경된것

--- src/main/java/com/team04/restaurant/RestaurantService.java
+++ src/main/java/com/team04/restaurant/RestaurantService.java
@@ -2,22 +2,32 @@ package com.team04.restaurant;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Service
 
 public class RestaurantService implements RestaurantDao {
-
+	@Autowired
+	private RestaurantDao restaurantDao;
+	
 	@Override
-	public List<RestaurantDto> selectrestaurantList() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<RestaurantDto> selectRestaurantList() {
+		return restaurantDao.selectRestaurantList();
 	}
 
 	@Override
 	public RestaurantDto selectRestaurant(String shopNo) {
-		// TODO Auto-generated method stub
-		return null;
+		return restaurantDao.selectRestaurant(shopNo);
 	}
+	
+	
+
+	
 
 }
