893f1d8313eddc7589635f21acdc61b006b37336
alwaysstudying <86540250+alwaysstudying@users.noreply.github.com>
2021-08-13 10:10:18 +0900

    restaurant

---
+++ src/main/java/com/team04/controller/RestaurantController.java
@@ -0,0 +1,43 @@
+package com.team04.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team04.restaurant.RestaurantDto;
+import com.team04.restaurant.RestaurantService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class RestaurantController {
+
+	@Autowired
+	public RestaurantService restaurantService;
+	
+	@RequestMapping("/registerForm")
+	public String esigterForm() {
+		return "registerForm";
+	}
+	
+	@RequestMapping("/shopList")
+	public String shopList(Model model, HttpSession session) {
+		// 요청에 대한 사용자 인증 및 권한 체킹 부분이 누락
+		// 인증여부 체크
+		if(session.getAttribute("shopNo") == null || session.getAttribute("dto") == null) {
+			model.addAttribute("message", "로그인 인증 후 이용하시기 바랍니다.");
+			return "shopList";
+		}
+		
+		List<RestaurantDto> list = restaurantService.selectRestaurantList();
+		log.info("RestaurantList: " + list.size());
+		model.addAttribute("list", list);
+		return "shopList";
+	}
+}
